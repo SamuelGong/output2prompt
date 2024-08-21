19b8ca7012ea2163e58e163fe4a7b965f96c48bb
Zhifeng Jiang <samuelgong2017@gmail.com>
2024-08-21 12:57:03 +0800

    A

---
+++ test_extract_data.py
@@ -0,0 +1,42 @@
+import os
+import time
+from datasets import load_from_disk
+from transformers import T5Tokenizer
+
+dataset = 'real_gpts'
+meta_map = {
+    'real_gpts': {
+        'src_path': 'datasets/test/real_gpts_arrow',
+        'tgt_path': 'extracted_datasets/test/real_gpts'
+    }
+}
+
+
+def decode_for_output2prompt(tokenizer, data):
+    for idx, token in enumerate(data):
+        if token == -100:
+            data[idx] = tokenizer.pad_token_id
+    text = tokenizer.decode(data, skip_special_tokens=True)
+    return text
+
+
+overall_start_time = time.perf_counter()
+tokenizer = T5Tokenizer.from_pretrained('t5-base')
+dataset_path = meta_map[dataset]['src_path']
+eval_ds = load_from_disk(dataset_path)
+
+target_dir = meta_map[dataset]['tgt_path']
+os.makedirs(target_dir, exist_ok=True)
+log_interval = 10
+for sample_idx, example in enumerate(eval_ds):
+    system_prompt_embed = eval_ds[sample_idx]["system_prompt"]
+    system_prompt = decode_for_output2prompt(tokenizer=tokenizer, data=system_prompt_embed)
+    save_path = os.path.join(target_dir, f"{sample_idx}.txt")
+    with open(save_path, 'w') as fout:
+        fout.write(system_prompt)
+    if sample_idx % log_interval == 0:
+        print(f"Processed {sample_idx}/{len(eval_ds)} samples")
+
+overall_end_time = time.perf_counter()
+overall_duration = overall_end_time - overall_start_time
+print(f"Done in {round(overall_duration, 2)}s.")
